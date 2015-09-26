51a6784c70f4d10757ee5fb1562404d3844c2196
Bryce Van Dyk <bryce@vandyk.net.nz>
2015-09-26 14:28:06 +1200

    Add body for make and solve functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+    inverse <- NULL
+    set <- function(newmat) {
+        mat <- newmat
+        inverse <- NULL
+    }
+    get <- function() mat
+    setinverse <- function(newinverse) inverse <<- newinverse
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat, ...) {
+    inverse <- mat$getinverse()
+    if(!is.null(inverse)) {
+        message("Getting cached inverse")
+        return(inverse)
+    }
+    data <- mat$get()
+    inverse <- solve(mat$get())
+    mat$setinverse(inverse)
+    inverse
 }
